564503c906634c8629be12f9f5fb3c378fdaa54a
daltoneotieno123@gmail.com <daltoneotieno123@gmail.com>
2024-07-30 16:09:40 +0300

    Write a script that computes the number of tasks completed by user id.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -12,30 +12,34 @@ const apiUrl = process.argv[2];
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error('Error making the request:', error);
-  } else if (response.statusCode !== 200) {
+    return;
+  }
+
+  if (response.statusCode !== 200) {
     console.error(`Error: Received status code ${response.statusCode}`);
-  } else {
-    try {
-      const todos = JSON.parse(body);
-      const userTasks = {};
+    return;
+  }
 
-      todos.forEach(todo => {
-        if (todo.completed) {
-          if (userTasks[todo.userId]) {
-            userTasks[todo.userId]++;
-          } else {
-            userTasks[todo.userId] = 1;
-          }
-        }
-      });
+  try {
+    const todos = JSON.parse(body);
+    const userTasks = {};
 
-      for (const userId in userTasks) {
-        if (Object.prototype.hasOwnProperty.call(userTasks, userId)) {
-          console.log(`User ${userId} has completed ${userTasks[userId]} tasks`);
+    todos.forEach(todo => {
+      if (todo.completed) {
+        if (userTasks[todo.userId]) {
+          userTasks[todo.userId]++;
+        } else {
+          userTasks[todo.userId] = 1;
         }
       }
-    } catch (parseError) {
-      console.error('Error parsing the response body:', parseError);
+    });
+
+    for (const userId in userTasks) {
+      if (Object.prototype.hasOwnProperty.call(userTasks, userId)) {
+        console.log(`User ${userId} has completed ${userTasks[userId]} tasks`);
+      }
     }
+  } catch (parseError) {
+    console.error('Error parsing the response body:', parseError);
   }
 });
