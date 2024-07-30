b6ce0a90da65c99ec1ff832e6bb8cc4a81b1eae8
daltoneotieno123@gmail.com <daltoneotieno123@gmail.com>
2024-07-30 15:14:16 +0300

    Write a script that writes a string to a file.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,15 +1,21 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 
-function writeFile (filePath, content) {
-  fs.writeFile(filePath, content, 'utf8', (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
+// Check if the correct number of arguments are provided
+if (process.argv.length !== 4) {
+  console.error('Usage: ./1-writeme.js <file_path> <string_to_write>');
+  process.exit(1);
 }
 
-if (process.argv.length === 4) {
-  const filePath = process.argv[2];
-  const content = process.argv[3];
-  writeFile(filePath, content);
+const filePath = process.argv[2]; // The file path
+const content = process.argv[3];  // The string to write
+
+// Write the content to the file with UTF-8 encoding
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err); // Print the error object if an error occurred
+  } else {
+    console.log('File written successfully'); // Success message
+  }
+});
