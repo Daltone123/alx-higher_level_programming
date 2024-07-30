af16ac686233306e2831a74f6c1178239b0a5726
daltoneotieno123@gmail.com <daltoneotieno123@gmail.com>
2024-07-30 16:02:11 +0300

    Write a script that computes the number of tasks completed by user id.

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: ./6-completed_tasks.js <API_URL>');
+  process.exit(1);
+}
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error making the request:', error);
+  } else if (response.statusCode !== 200) {
+    console.error(`Error: Received status code ${response.statusCode}`);
+  } else {
+    try {
+      const todos = JSON.parse(body);
+      const userTasks = {};
+
+      todos.forEach(todo => {
+        if (todo.completed) {
+          if (userTasks[todo.userId]) {
+            userTasks[todo.userId]++;
+          } else {
+            userTasks[todo.userId] = 1;
+          }
+        }
+      });
+
+      for (const userId in userTasks) {
+        if (Object.prototype.hasOwnProperty.call(userTasks, userId)) {
+          console.log(`User ${userId} has completed ${userTasks[userId]} tasks`);
+        }
+      }
+    } catch (parseError) {
+      console.error('Error parsing the response body:', parseError);
+    }
+  }
+});
